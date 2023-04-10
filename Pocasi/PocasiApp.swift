a7078f91d8cd86cc89239bbeba00434620ff8b04
OndraHa <Ondrejhadraba@gmail.com>
2023-04-10 21:44:14 +0200

    Initial Commit

---
+++ Pocasi/PocasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocasiApp.swift
+//  Pocasi
+//
+//  Created by Ondřej Hadraba on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct PocasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
