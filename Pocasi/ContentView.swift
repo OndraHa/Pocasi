6cbcacd8ab1c6ad7eef130448adb720dddcb94b7
OndraHa <Ondrejhadraba@gmail.com>
2023-04-11 10:07:16 +0200

    Mapa

--- Pocasi/ContentView.swift
+++ Pocasi/ContentView.swift
@@ -5,17 +5,14 @@
 //  Created by Ondřej Hadraba on 10.04.2023.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    @State private var mapView = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 49.743706, longitude: 15.339102), span: MKCoordinateSpan(latitudeDelta: 7, longitudeDelta: 7))
+    var body: some View{
+        
+    Map(coordinateRegion: $mapView)
     }
 }
 
@@ -24,3 +21,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+//https://api.openweathermap.org/data/3.0/onecall?lat=49.8209&lon=18.2625&exclude=minutely,hourly,alerts&appid=5a5769988061d2a4b575a39b6c9e3a99&units=metric
---
+++ Pocasi/Locations.swift
@@ -0,0 +1,8 @@
+//
+//  Locations.swift
+//  Pocasi
+//
+//  Created by Ondřej Hadraba on 11.04.2023.
+//
+
+import Foundation
