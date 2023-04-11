61d85cc53789291ce16bae53de3b3cb4c6d81d49
OndraHa <Ondrejhadraba@gmail.com>
2023-04-11 14:14:23 +0200

    server call

--- Pocasi/DetailView.swift
+++ Pocasi/DetailView.swift
@@ -13,10 +13,31 @@ struct DetailView: View {
         Text(location.name)
         Text("\(location.latitude)")
         Text("\(location.longitude)")
+            .onAppear{
+                dataDownload(lat: location.latitude, lon: location.longitude)
+            }
     }
     func dataDownload(lat: Double, lon: Double){
         let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=\(lat)&lon=\(lon)&exclude=minutely,hourly,alerts&appid=\(APIkey.appID)&units=metric"
+        let url = URL(string: urlString)!
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+            guard let data = data else {
+                print("Missing data!")
+                return
+            }
+            
+            if let json = try? JSONSerialization.jsonObject(with: data){
+                print(json)
+            }
+            
+        }
+        task.resume()
     }
+        
 }
 
 struct DetailView_Previews: PreviewProvider {
